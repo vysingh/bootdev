349c91ec27e92b5f8ab1ee6dee407cf6c96d9a6b
Vy Singh <44628175+vysingh@users.noreply.github.com>
2026-06-27 17:11:06 +1000

    Create leafnode.py

---
+++ src/leafnode.py
@@ -0,0 +1,27 @@
+from htmlnode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+
+        if self.tag is None:
+            return self.value
+
+        return (
+            f"<{self.tag}{self.props_to_html()}>"
+            f"{self.value}"
+            f"</{self.tag}>"
+        )
+
+    def __repr__(self):
+        return (
+            f"LeafNode("
+            f"tag={self.tag}, "
+            f"value={self.value}, "
+            f"props={self.props})"
+        )
