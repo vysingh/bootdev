3f9e6609880d70f86c77994d0469a94f37234b62
Vy Singh <44628175+vysingh@users.noreply.github.com>
2026-06-27 17:11:47 +1000

    Update test_leafnode.py

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -1,1 +1,40 @@
-test_leafnode.py
+import unittest
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(
+            node.to_html(),
+            "<p>Hello, world!</p>",
+        )
+
+    def test_leaf_to_html_link(self):
+        node = LeafNode(
+            "a",
+            "Click me!",
+            {"href": "https://www.google.com"},
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+
+    def test_leaf_no_tag(self):
+        node = LeafNode(None, "Raw text")
+        self.assertEqual(
+            node.to_html(),
+            "Raw text",
+        )
+
+    def test_leaf_no_value(self):
+        node = LeafNode("p", None)
+
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+
+if __name__ == "__main__":
+    unittest.main()
